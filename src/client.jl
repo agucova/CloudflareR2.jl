42a2d1bfa0f2132aa1ae41714d9e783d4d0f04d8
Agustín Covarrubias <agucova@uc.cl>
2023-04-10 04:02:27 -0400

    feat: working wrapper

--- src/client.jl
+++ src/client.jl
@@ -7,14 +7,14 @@ A configuration object allowing to use the AWS S3 interface from AWS.jl and AWSS
 ## Constructors
 ```julia
 # By passing only your Cloudflare account ID
-R2Config(account_id; access_key_id, secret_access_key)
-R2Config(account_id, creds)
+R2Config(account_id; access_key_id, secret_access_key, region)
+R2Config(account_id, creds; region)
 
 # By passing the endpoint directly
 using URIs
 
-R2Config(URI(endpoint); access_key_id, secret_access_key)
-R2Config(URI(endpoint), creds)
+R2Config(URI(endpoint); access_key_id, secret_access_key, region)
+R2Config(URI(endpoint), creds; region)
 ```
 
 ## Arguments
@@ -22,21 +22,25 @@ R2Config(URI(endpoint), creds)
 - `endpoint`: Your Cloudflare S3 API endpoint.
 - `creds`: An `AWSCredentials` object providing the credentials to access R2.
 - `access_key_id`: An access key ID for Cloudflare R2. This will use `R2_ACCESS_KEY_ID` or `AWS_ACCESS_KEY_ID` if
-    available, or otherwise set to an empty string.
+    available.
 - `secret_access_key`: A secret access key for Cloudflare R2. This will use `R2_SECRET_ACCESS_KEY` or `AWS_SECRET_ACCESS_KEY`
-    if available, or otherwise set to an empty string.
+    if available.
+- `region`: The region to use. Defaults to `"auto"`.
+    Valid values are `"auto"`, `"wnam"`, `"enam"`, `"weur"`, `"eeur"`, and `"apac"`.
 
 ## Examples
 ```julia
 using R2
 
-cfg = R2Config("YOUR_ACCOUNT_ID")
+# tokens are read from environment
+# region set to auto
+config = R2Config("YOUR_ACCOUNT_ID")
 
 # using the AWS S3 API
-s3_list_buckets(cfg)
+s3_list_buckets(config)
 
 # using the S3Path interface
-path = S3Path("s3://bucket-name", config=cfg)
+path = S3Path("s3://bucket-name", config=config)
 readdir(path)
 ```
 """
@@ -47,8 +51,20 @@ struct R2Config <: AbstractAWSConfig
     creds::AWSCredentials
 end
 
-function R2Config(endpoint::URI, creds::AWSCredentials)
-    R2Config(endpoint, "cloudflare-global", creds)
+CLOUDFLARE_REGIONS = [
+    "auto",
+    "wnam",
+    "enam",
+    "weur",
+    "eeur",
+    "apac"
+]
+
+function R2Config(endpoint::URI, creds::AWSCredentials; region::AbstractString="auto")
+    if region ∉ CLOUDFLARE_REGIONS
+        throw(ArgumentError("region must be one of $(CLOUDFLARE_REGIONS)."))
+    end
+    R2Config(endpoint, region, creds)
 end
 
 function isalnum(s::AbstractString)::Bool
@@ -58,29 +74,30 @@ function isalnum(s::AbstractString)::Bool
     return true
 end
 
-function R2Config(account_id::AbstractString, creds::AWSCredentials)
+function R2Config(account_id::AbstractString, creds::AWSCredentials; region::AbstractString="auto")
     if !isalnum(account_id)
         throw(ArgumentError("account_id must be alphanumeric. \
         If you want to pass the endpoint directly, pass a URI object instead."))
     end
     endpoint = URI("https://$(account_id).r2.cloudflarestorage.com")
-    R2Config(URI(endpoint), creds)
+    R2Config(URI(endpoint), creds; region=region)
 end
 
-function get_access_key_id()
-    return @something get(ENV, "R2_ACCESS_KEY_ID", missing), get(ENV, "AWS_ACCESS_KEY_ID", "")
+function get_access_key_id()::AbstractString
+    return @something get(ENV, "R2_ACCESS_KEY_ID", nothing) get(ENV, "AWS_ACCESS_KEY_ID", nothing) error("No access key ID found.")
 end
 
 function get_secret_access_key()
-    return @something get(ENV, "R2_SECRET_ACCESS_KEY", missing), get(ENV, "AWS_SECRET_ACCESS_KEY", "")
+    return @something get(ENV, "R2_SECRET_ACCESS_KEY", nothing) get(ENV, "AWS_SECRET_ACCESS_KEY", nothing) error("No secret access key found.")
 end
 
 function R2Config(
-        id_or_endpoint::Union{AbstractString, URI};
-        access_key_id::AbstractString=get_access_key_id(),
-        secret_access_key::AbstractString=get_secret_access_key(),
-    )
-    R2Config(id_or_endpoint, AWSCredentials(access_key_id, secret_access_key, "", ""))
+    id_or_endpoint::Union{AbstractString,URI};
+    access_key_id::AbstractString=get_access_key_id(),
+    secret_access_key::AbstractString=get_secret_access_key(),
+    region::AbstractString="auto"
+)
+    R2Config(id_or_endpoint, AWSCredentials(access_key_id, secret_access_key, "", ""); region=region)
 end
 
 AWS.region(cfg::R2Config) = cfg.region
